4771f064663e9a9463f3e1dc0897651b68400746
MohamedAgbaryh <mistertroll2008@gmail.com>
2026-01-19 03:20:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openPage(id) {
+  document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+}
+
+function goHome() {
+  openPage('home');
+}
